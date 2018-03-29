7c8ea7198faea120b1d1aa22ece5b368a6273cbc
dfish292 <37908251+dfish292@users.noreply.github.com>
2018-03-29 10:57:04 -0500

    Add files via upload
    Lab 11 - Github

---
+++ EmployeeDictionary/EmployeeDictionary/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDictionary
+{
+    class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public int ID;
+        public string Print()
+        {
+            return ID + ":  " + FirstName + " " + LastName;
+        }
+    }
+}
---
+++ EmployeeDictionary/EmployeeDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, Employee> EmployeeDictionary = new Dictionary<int, Employee>();
+
+            EmployeeDictionary.Add(101, new Employee { ID = 101, FirstName = "Jill", LastName = "Ruby" });
+            EmployeeDictionary.Add(102, new Employee { ID = 102, FirstName = "Greg", LastName = "Davi" });
+            EmployeeDictionary.Add(103, new Employee { ID = 103, FirstName = "Clay", LastName = "Cran" });
+
+            Console.WriteLine(EmployeeDictionary[101].Print());
+            Console.WriteLine(EmployeeDictionary[102].Print());
+            Console.WriteLine(EmployeeDictionary[103].Print());
+
+            Console.ReadLine();
+        }
+    }
+}
